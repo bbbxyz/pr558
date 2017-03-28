6cccecd69e951624de422e3082e067db873fc64b
bbbxyz <bbbxyz@users.noreply.github.com>
2017-03-28 19:14:09 -0400

    added a few examples

--- test.m
+++ test.m
@@ -1,7 +1,15 @@
-%L = imread('ct_scan.jpg');
-L=rgb2gray(imread('wall.png'));
+%L = 255-imread('ct_scan.jpg');
+L=rgb2gray(imresize(imread('wall.png'), 1));
 [w,h]=size(L);
-%filtered = zeros(10,w,h);
-min_range=2;
-max_range=4;
-imshow(vness(L,4));
+
+scale1 = vness(L,1);
+scale2 = vness(L,2);
+scale3 = vness(L,3);
+scale4 = vness(L,4);
+scale5 = vness(L,5);
+merged_scales = (scale1+scale2+scale3+scale4+scale5)/5;
+subplot(1,2,1);
+imshow(merged_scales);
+subplot(1,2,2);
+imshow(L);
+imwrite(merged_scales, 'wall_filtered.png');
--- vness.m
+++ vness.m
@@ -9,7 +9,7 @@ R_b = zeros(w,h);
 v = zeros(w,h);
 alpha = 0.5;
 beta = 0.5;
-c = 2;
+c = 0.5 * max(max(max([Lxx,Lxy,Lyy])));
 for i =[1:w],
     for j = [1:h],
         A = [Lxx(i,j), Lxy(i,j);Lxy(i,j), Lyy(i,j) ];
@@ -18,7 +18,7 @@ for i =[1:w],
         e2 = e(2);
         eigens(i,j,:) = e(:); 
         S(i,j) = sqrt(e1.^2 + e2.^2);
-        R_b(i,j) = abs(e1) ./ sqrt(abs(e2));
+        R_b(i,j) = e1 / e2;
         if(e2>0),
             v(i,j) = exp(-R_b(i,j).^2 /(2*beta^2)) * (1-exp(-S(i,j)^2 /(2*c^2)));
         end
